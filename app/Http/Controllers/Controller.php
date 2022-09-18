250e2d7c5be36394103215850fee13aeb4634162
Mike <bavdysmyh@ukr.net>
2022-09-18 13:02:21 +0200

    documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        return response()->json([
+            'Welcome to MYTHERESA API',
+            'routes' => [
+                'products' => [
+                    'list' => '/products',
+                    'example' => '/products?category=boots&priceLessThan=100000',
+                    'attributes' => [
+                        'category' => 'Can be filtered by category as a query string parameter',
+                        'priceLessThan' => 'Can be filtered by priceLessThan as a query string parameter, '
+                            . 'this filter applies before discounts are applied and will show products with prices '
+                            . 'lesser than or equal the value provided',
+                    ],
+                ]
+            ]
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,12 @@
 | Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
+| Here is where you can register all the routes for an application.
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
 */
+
+$router->get('/', 'Controller@index');
 
 $router->get('products', 'ProductController@index');
